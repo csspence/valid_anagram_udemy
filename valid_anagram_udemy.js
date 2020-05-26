1ebdf3f6c64ef5629439368b46be806afcd6f417
Crew Spence <csspence123@gmail.com>
2020-05-26 06:51:48 -0500

    Wrote working solution.

--- valid_anagram_udemy.js
+++ valid_anagram_udemy.js
@@ -10,7 +10,29 @@ validAnagram('aaz', 'zza') // false
 validAnagram('anagram', 'nagaram') // true
 validAnagram('rat', 'car') // false
 validAnagram('qwerty', 'qeywrt') // true
+*/
 
 const validAnagram = (str1, str2) => {
-  
+  if(str1.length !== str2.length) {
+    return false;
+  }
+  const counter = {};
+  for(let i = 0; i < str1.length; i++) {
+    if(counter[str1[i]] === undefined) {
+      counter[str1[i]] = 1;
+    } else {
+      counter[str1[i]]++;
+    }
+  }
+  for(let h = 0; h < str2.length; h++) {
+    if(counter[str2[h]] === undefined) {
+      return false;
+    }
+    if(counter[str2[h]] === 0) {
+      return false;
+    }
+    counter[str2[h]]--;
+  }
+
+  return true;
 }
